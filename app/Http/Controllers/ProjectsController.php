13ca8b4a4949c96ce9e8c639dcdacff51385d6e6
vagrant <vagrant@homestead>
2019-12-12 09:55:31 +0000

    First unit test

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+
+class ProjectsController extends Controller
+{
+    public function index(){
+        $projects = Project::all();
+
+        return view("projects.index", ["projects" => $projects]);
+    }
+
+    public function store(){
+
+        $attributes = request()->validate([
+            "title" => "required",
+            "description" => "required"
+            ]);
+
+        Project::create($attributes);
+
+        return redirect("/projects");
+    }
+
+    public function show(Project $project){
+
+        return view("projects.show", ["project" => $project]);
+    }
+}
