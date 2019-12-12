2ba8a9098408e6dbfb31d23f139c95bcde0d3917
vagrant <vagrant@homestead>
2019-12-12 11:35:22 +0000

    Added auth

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -9,6 +9,9 @@ use Illuminate\Database\Eloquent\Factory;
 $factory->define(Project::class, function (Faker $faker) {
     return [
         "title" => $faker->sentence,
-        "description" => $faker->paragraph
+        "description" => $faker->paragraph,
+        "owner_id" => function(){
+            return factory(App\User::class)->create()->id;
+        }
     ];
 });
